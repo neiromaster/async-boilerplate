919c5528febc1f9df99de4d430b21c2aa127e704
neiromaster <parrysh@mail.ru>
2019-04-25 10:24:19 +0300

    add animejs and update packages

--- vue.config.js
+++ vue.config.js
@@ -13,14 +13,14 @@ module.exports = {
 
   pluginOptions: {
     quasar: {
-      treeShake: true
+      treeShake: true,
     },
     i18n: {
       locale: 'ru',
       fallbackLocale: 'en',
       localeDir: 'locales',
-      enableInSFC: true
-    }
+      enableInSFC: true,
+    },
   },
 
   transpileDependencies: [
